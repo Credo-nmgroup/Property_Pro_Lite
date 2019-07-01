8172e02f47aaefc098703e97af4c303f6fa41ffb
credo-nmgroup <aguetec@gmail.com>
2019-07-01 17:59:21 +0000

    feature(user interface elements) test the javascript code with ESlint

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,26 +1,30 @@
 // cuurency enter
-var cuurencyInput = document.querySelector('input[ type = "currency"]');
+var currencyInput = document.querySelector('input[ type = "currency"]');
 var currency = 'USD';
 
 currencyInput.addEventListener('focus', onFocus);
-cuurencyInput.addEventListener('blur', onBlur);
+currencyInput.addEventListener('blur', onBlur);
 
 const localStringToNumber = s =>{
-	return Number(String(s).replace(/[^0-9.-]+/g, ""));
-}
+    return Number(String(s).replace(/[^0-9.-]+/g, ''));
+};
 
 const onFocus = e =>{
-	var value = e.target.value;
-	e.target.value = value ? localStringToNumber(value) : '';
-}
+    var value = e.target.value;
+    e.target.value = value ? localStringToNumber(value) : '';
+};
 
 const onBlur = e =>{
-	var value = e.target.value;
-   const options = {
-   	maximumFractionDigits : 2,
-   	currency              : currency,
-   	style                 : "cuurency",
-   	currencyDisplay       : "symbol"
-   }
-   e.target.value = value ? localStringToNumber(value).toLocaleString(undefined, options) : ''; 
-}
+    var value = e.target.value;
+    const options = {
+        maximumFractionDigits : 2,
+       
+        currency              : currency,
+       
+        style                 : 'cuurency',
+       
+        currencyDisplay       : 'symbol'
+          
+    };
+    e.target.value = value ? localStringToNumber(value).toLocaleString(undefined, options) : ''; 
+};
