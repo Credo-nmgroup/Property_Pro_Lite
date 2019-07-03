7a6acc1930883072f81059ba43ba7f27efbc559a
credo-nmgroup <aguetec@gmail.com>
2019-07-03 16:36:41 +0000

    add models for property, flag and users

---
+++ server/models/property_model.js
@@ -0,0 +1,19 @@
+const property = [
+    {
+    id :​ ​1,
+    owner : 34568099,
+    status :​ "available",
+    price : 450,
+​    state :  "state",
+​    city  : "city",
+​    address​ : "address",
+    type  : "type"​,
+​    created_on : new Date(),
+​    image_url  : "image-url",​
+    category : "category",
+    title : "title",
+    description : "description"
+    }
+]
+
+export default property;
---
+++ server/models/user_model.js
@@ -0,0 +1,13 @@
+const users = [
+    {
+    id :​ ​1,
+    email : "xyz@domain.com",
+    first_name: "firstname",
+    last_name​:​ "lastname",
+    password :"password",
+    phoneNumber​:​ ​"phoneNumber",
+    address :"address" ,
+    is_admin:  true
+    }
+];
+export default users;
